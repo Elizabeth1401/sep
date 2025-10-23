52ca57f8cf67767893c3189854a77f88c790fe26
Teo <teodora.cristiana.stoicescu@gmail.com>
2025-10-23 11:18:27 +0200

    Initial commit
    Took 27 minutes

---
+++ src/main/java/com/findmyclub/model/Club.java
@@ -0,0 +1,43 @@
+package com.findmyclub.model;
+
+import jakarta.persistence.*;
+
+@Entity @Table(name = "club") public class Club
+{
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Integer id;
+
+  @Column(nullable = false) private String name;
+
+  private String location;
+
+  // Getters and Setters
+  public Integer getId()
+  {
+    return id;
+  }
+
+  public void setId(Integer id)
+  {
+    this.id = id;
+  }
+
+  public String getName()
+  {
+    return name;
+  }
+
+  public void setName(String name)
+  {
+    this.name = name;
+  }
+
+  public String getLocation()
+  {
+    return location;
+  }
+
+  public void setLocation(String location)
+  {
+    this.location = location;
+  }
+}
---
+++ src/main/java/com/findmyclub/server/ServerPlaceholder.java
@@ -0,0 +1,5 @@
+package com.findmyclub.server;
+
+public class ServerPlaceholder
+{
+}

92457fbf9f11e4e92ec3a830235a766e864725aa
Elizabeth1401 <140805762+Elizabeth1401@users.noreply.github.com>
2025-10-26 19:27:49 +0100

    I added UserController and DTO

--- src/auth-service/java/com/findmyclub/controller/UserController.java
+++ src/auth-service/java/com/findmyclub/controller/UserController.java
@@ -1,13 +1,12 @@
 package com.findmyclub.controller;
 
+import com.findmyclub.DTO.UserDTO;
 import com.findmyclub.model.User;
 import com.findmyclub.repositories.UserRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/auth")
@@ -19,8 +18,29 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    @GetMapping
-    public List<User> getAll() {
-        return userRepository.findAll();
+    //create a new user
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public User create(@RequestBody UserDTO userDTO) {
+        User user = new User(userDTO.username, userDTO.password);
+        //for now we do not encrypt username and password it will be in the future
+        return userRepository.save(user);
     }
+
+    //login
+    @PostMapping ("/login")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseEntity<String> login(@RequestBody UserDTO userDTO) {
+        var user  = userRepository.findByUsername(userDTO.username);
+        if (user.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+        }
+
+        if (user.get().getPassword().equals(userDTO.password)) {
+            return ResponseEntity.ok("Logged in");
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
 }
